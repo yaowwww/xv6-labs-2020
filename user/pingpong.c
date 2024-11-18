a8b85328abc2c8fbd66e8bff1e71e87e2d4802e7
yaowwww <2313421391@qq.com>
2024-11-18 21:43:36 +0800

    修改了pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,39 +1,106 @@
+// #include "kernel/types.h"
+// //#include "kernel/stat.h"
+// #include "user/user.h"
+
+// //0是read端，1是write端
+// int main(int argc, char *argv[])       {
+//     int p1[2],p2[2];       //两个文件描述符
+//     pipe(p1);           //父管道
+//     pipe(p2);           //子管道
+//     char buf[8];         //缓冲区
+// //int exit_status = 0;   //判断是否有异常退出情况
+
+//     if(fork() < 0){   //出错
+// 	fprintf(2, "fork() error!\n");
+// 	close(p1[0]);
+// 	close(p1[1]);
+// 	close(p2[0]);
+// 	close(p2[1]);
+// 	exit(1);
+//     }else if(fork() == 0){       //fork()返回0即为子进程，否则为父进程
+// 	close(p1[1]);
+// 	close(p2[0]);
+// 	read(p1[0],buf,4);     //从父管道的读端读出父的数据写进缓冲区
+// 	printf("%d:receive %s\n",getpid(),buf);
+// 	write(p2[1],"pong",strlen("pong"));       //子管道写
+// 	close(p1[0]);
+// 	close(p2[1]);
+//     }else{                  //父进程
+//         close(p1[0]);
+// 	close(p2[1]);
+// 	write(p1[1],"ping",strlen("ping"));
+// 	wait(0);   //调用 wait() 函数等待子进程完成操作后退出，传入参数 0 或者 NULL
+// 	read(p2[0],buf,4);
+// 	printf("%d:receive %s\n",getpid(),buf);
+// 	close(p1[1]);
+// 	close(p2[0]);
+//     }
+//     exit(0);
+// }
 #include "kernel/types.h"
-//#include "kernel/stat.h"
 #include "user/user.h"
 
-//0是read端，1是write端
-int main(int argc, char *argv[])       {
-    int p1[2],p2[2];       //两个文件描述符
-    pipe(p1);           //父管道
-    pipe(p2);           //子管道
-    char buf[8];         //缓冲区
-//int exit_status = 0;   //判断是否有异常退出情况
+#define RD 0 //pipe的read端
+#define WR 1 //pipe的write端
 
-    if(fork() < 0){   //出错
-	fprintf(2, "fork() error!\n");
-	close(p1[0]);
-	close(p1[1]);
-	close(p2[0]);
-	close(p2[1]);
-	exit(1);
-    }else if(fork() == 0){       //fork()返回0即为子进程，否则为父进程
-	close(p1[1]);
-	close(p2[0]);
-	read(p1[0],buf,4);     //从父管道的读端读出父的数据写进缓冲区
-	printf("%d:receive %s\n",getpid(),buf);
-	write(p2[1],"pong",strlen("pong"));       //子管道写
-	close(p1[0]);
-	close(p2[1]);
-    }else{                  //父进程
-        close(p1[0]);
-	close(p2[1]);
-	write(p1[1],"ping",strlen("ping"));
-	wait(0);   //调用 wait() 函数等待子进程完成操作后退出，传入参数 0 或者 NULL
-	read(p2[0],buf,4);
-	printf("%d:receive %s\n",getpid(),buf);
-	close(p1[1]);
-	close(p2[0]);
+int main(int argc, char const *argv[]) {
+    char buf = 'P'; //用于传送的字节
+
+    int fd_c2p[2]; //子进程->父进程
+    int fd_p2c[2]; //父进程->子进程
+    pipe(fd_c2p);
+    pipe(fd_p2c);
+
+    int pid = fork();
+    int exit_status = 0;
+
+    if (pid < 0) {
+        fprintf(2, "fork() error!\n");
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+        exit(1);
+    } else if (pid == 0) { //子进程
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        if (read(fd_p2c[RD], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "child read() error!\n");
+            exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received ping\n", getpid());
+        }
+
+        if (write(fd_c2p[WR], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "child write() error!\n");
+            exit_status = 1;
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        exit(exit_status);
+    } else { //父进程
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        if (write(fd_p2c[WR], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent write() error!\n");
+            exit_status = 1;
+        }
+
+        if (read(fd_c2p[RD], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent read() error!\n");
+            exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received pong\n", getpid());
+        }
+
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        exit(exit_status);
     }
-    exit(0);
 }
+
--- user/primes.c
+++ user/primes.c
@@ -1,67 +1,83 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-/*
- * Run as a prime-number processor
- * the listenfd is from your left neighbor
+#define RD 0
+#define WR 1
+
+const uint INT_LEN = sizeof(int);
+
+/**
+ * @brief 读取左邻居的第一个数据
+ * @param lpipe 左邻居的管道符
+ * @param pfirst 用于存储第一个数据的地址
+ * @return 如果没有数据返回-1,有数据返回0
  */
-void runprocess(int listenfd) {
-  int my_num = 0;
-  int forked = 0;
-  int passed_num = 0;
-  int pipes[2];
-  while (1) {
-    int read_bytes = read(listenfd, &passed_num, 4);
-
-    // left neighbor has no more number to provide
-    if (read_bytes == 0) {
-      close(listenfd);
-      if (forked) {
-        // tell my children I have no more number to offer
-        close(pipes[1]);
-        // wait my child termination
-        int child_pid;
-        wait(&child_pid);
-      }
-      exit(0);
-    }
-
-    // if my initial read
-    if (my_num == 0) {
-      my_num = passed_num;
-      printf("prime %d\n", my_num);
-    }
-
-    // not my prime multiple, pass along
-    if (passed_num % my_num != 0) {
-      if (!forked) {
-        pipe(pipes);
-        forked = 1;
-        int ret = fork();
-        if (ret == 0) {
-          // i am the child
-          close(pipes[1]);
-          close(listenfd);
-          runprocess(pipes[0]);
-        } else {
-          // i am the parent
-          close(pipes[0]);
-        }
-      }
-
-      // pass the number to right neighbor
-      write(pipes[1], &passed_num, 4);
-    }
+int lpipe_first_data(int lpipe[2], int *dst)
+{
+  if (read(lpipe[RD], dst, sizeof(int)) == sizeof(int)) {
+    printf("prime %d\n", *dst);
+    return 0;
   }
+  return -1;
 }
 
-int main(int argc, char *argv[]) {
-  int pipes[2];
-  pipe(pipes);
-  for (int i = 2; i <= 35; i++) {
-    write(pipes[1], &i, 4);
+/**
+ * @brief 读取左邻居的数据，将不能被first整除的写入右邻居
+ * @param lpipe 左邻居的管道符
+ * @param rpipe 右邻居的管道符
+ * @param first 左邻居的第一个数据
+ */
+void transmit_data(int lpipe[2], int rpipe[2], int first)
+{
+  int data;
+  // 从左管道读取数据
+  while (read(lpipe[RD], &data, sizeof(int)) == sizeof(int)) {
+    // 将无法整除的数据传递入右管道
+    if (data % first)
+      write(rpipe[WR], &data, sizeof(int));
   }
-  close(pipes[1]);
-  runprocess(pipes[0]);
+  close(lpipe[RD]);
+  close(rpipe[WR]);
+}
+
+/**
+ * @brief 寻找素数
+ * @param lpipe 左邻居管道
+ */
+void primes(int lpipe[2])
+{
+  close(lpipe[WR]);
+  int first;
+  if (lpipe_first_data(lpipe, &first) == 0) {
+    int p[2];
+    pipe(p); // 当前的管道
+    transmit_data(lpipe, p, first);
+
+    if (fork() == 0) {
+      primes(p);    // 递归的思想，但这将在一个新的进程中调用
+    } else {
+      close(p[RD]);
+      wait(0);
+    }
+  }
+  exit(0);
+}
+
+int main(int argc, char const *argv[])
+{
+  int p[2];
+  pipe(p);
+
+  for (int i = 2; i <= 35; ++i) //写入初始数据
+    write(p[WR], &i, INT_LEN);
+
+  if (fork() == 0) {
+    primes(p);
+  } else {
+    close(p[WR]);
+    close(p[RD]);
+    wait(0);
+  }
+
   exit(0);
 }
